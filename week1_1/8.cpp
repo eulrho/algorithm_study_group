fa16593c43de1fff70703b6fbf8a8fc0075390b8
eulrho <sunriselss19@gmail.com>
2023-10-02 23:09:35 +0900

    week1_1.8 A+B -7

---
+++ week1_1/8.cpp
@@ -0,0 +1,15 @@
+// A+B -7 bj.11021
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int T, A, B;
+
+    scanf("%d", &T);
+    for (int i=0; i<T; i++)
+    {
+        scanf("%d %d", &A, &B);
+        printf("Case #%d: %d\n", i+1, A+B);
+    }
+    return (0);
+}
