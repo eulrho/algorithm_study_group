fb9da3d071f309b99229d00474c2196216221dad
eulrho <sunriselss19@gmail.com>
2023-10-02 23:10:33 +0900

    week1_1.11 그대로 출력하기 2

---
+++ week1_1/11.cpp
@@ -0,0 +1,15 @@
+// 그대로 출력하기 2 bj.11719
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string arr;
+
+    for (int i=0; i<100; i++)
+    {
+        getline(cin, arr);
+        cout << arr << "\n";
+    }
+    return (0);
+}
