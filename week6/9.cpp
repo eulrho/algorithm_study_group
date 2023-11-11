177be23a77986eb068c8df4595383ea99b25e3c3
eulrho <sunriselss19@gmail.com>
2023-11-11 18:21:06 +0900

    week6.9 완전 이진 트리

---
+++ week6/9.cpp
@@ -0,0 +1,41 @@
+// 완전 이진 트리 bj.9934
+#include <iostream>
+#include <cmath>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int idx=1;
+
+void find_node(int *building, int *result, int node, int max_node)
+{
+    if (2 * node <= max_node)
+        find_node(building, result, 2 * node, max_node);
+    result[node] = building[idx];
+    idx += 1;
+    if (2 * node + 1 <= max_node)
+        find_node(building, result, 2 * node + 1, max_node);
+}
+
+int main()
+{
+    int level=1, mult=1, k, size;
+    cin >> k;
+    size = (int)pow(2, k) - 1;
+    int *building = new int[size+1];
+    int *result = new int[size+1];
+
+    for (int i=1; i<=size; i++) cin >> building[i];
+    find_node(building, result, 1, size);
+    for (int i=1; i<=size; i++) {
+        cout << result[i];
+        if (level == i) {
+            cout << '\n';
+            level += 2*mult;
+            mult++;
+        }
+        else cout << ' ';
+    }
+    delete []building;
+    delete []result;
+    return (0);
+}
