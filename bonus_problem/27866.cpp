37639847d57ca3aafadb1a6da35d360ef766c9f1
eulrho <sunriselss19@gmail.com>
2023-12-03 01:41:01 +0900

    bonus.27866 문자와 문자열

---
+++ bonus_problem/27866.cpp
@@ -0,0 +1,14 @@
+// 문자와 문자열
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    int idx;
+
+    cin >> str >> idx;
+    cout << str[idx - 1];
+    return (0);
+}
