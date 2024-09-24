fcc50c24eb03c3a5f720d3cca6e4a72dc4045a8c
eulrho <sunriselss19@gmail.com>
2024-09-24 18:43:35 +0900

    24_9_week4.1 이진 검색 트리

---
+++ 24_9_week4/1.cpp
@@ -0,0 +1,36 @@
+// 이진 검색 트리
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int preorder[10001];
+int postorder[10001];
+
+int post_idx = 1;
+
+void ft_postorder(int index, int size, int range_max)
+{
+	int right_node = index + 1;
+
+	while (right_node < range_max && preorder[index] > preorder[right_node])
+		right_node++;
+	if (index + 1 < size && preorder[index] > preorder[index + 1])
+		ft_postorder(index + 1, size, right_node);
+	if (right_node < range_max)
+		ft_postorder(right_node, size, range_max);
+	postorder[post_idx++] = preorder[index];
+}
+
+int main()
+{
+	fast;
+	int node, size=1;
+
+	while (cin >> node)
+		preorder[size++] = node;
+	ft_postorder(1, size, size);
+	for (int i=1; i<size; i++) {
+		cout << postorder[i] << '\n';
+	}
+	return 0;
+}
