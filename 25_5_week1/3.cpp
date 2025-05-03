5f6bb66f822a0c4b8bb6d64c89d7cf09f783310b
eulrho <sunriselss19@gmail.com>
2025-05-03 22:43:07 +0900

    [Feat] time complexity

--- 25_5_week1/3.cpp
+++ 25_5_week1/3.cpp
@@ -8,11 +8,11 @@ vector<int> solution(vector<int> numbers) {
 	vector<int> answer;
 
 	int size = (int)numbers.size();
-	for (int i=0; i<size; i++) {
+	for (int i=0; i<size; i++) { // O(n^2)
 		for (int j=i+1; j<size; j++)
 			answer.push_back(numbers[i] + numbers[j]);
 	}
-	sort(answer.begin(), answer.end());
-	answer.erase(unique(answer.begin(), answer.end()), answer.end());
+	sort(answer.begin(), answer.end()); // O(nlogn)
+	answer.erase(unique(answer.begin(), answer.end()), answer.end()); // // O(n^2)
 	return answer;
 }
